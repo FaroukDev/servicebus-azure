83bd72ccad606a745266f731389a8a4af98b8078
Mohamed ABDENNEBI <mohamed.abdennebi@exakis-nelite.com>
2022-02-02 21:33:12 +0100

    features- install ioredis and connexion database Redis

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const Redis = require('ioredis');
+
 module.exports = async function(context, mySbMsg) {
     context.log('JavaScript ServiceBus queue trigger function processed message', mySbMsg);
+
+    const redis = new Redis({
+        port: 6380, // Redis port
+        host: "XXXX.redis.cache.windows.net", // Redis host
+        password: "XXXX",
+        timeout:300,
+          tls: {
+              servername: "XXXX.redis.cache.windows.net",
+          },
+        database: 0,
+      });
+
+
 };
