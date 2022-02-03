45b50a9518d31164f948e3dbc62813c4c915c681
FaroukDev <fa.elayache1@gmail.com>
2022-02-03 12:03:41 +0100

    add function subgetbalance and addgetbalance to script

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ module.exports = async function(context, mySbMsg) {
 
     const redis = new Redis({
         port: 6380, // Redis port
-        host: "XXXX.redis.cache.windows.net", // Redis host
-        password: "XXXX",
+        host: "XXX.redis.cache.windows.net", // Redis host
+        password: "XXX",
         timeout:300,
           tls: {
-              servername: "XXXX.redis.cache.windows.net",
+              servername: "redis-balance-account.redis.cache.windows.net",
           },
         database: 0,
       });
@@ -21,16 +21,35 @@ module.exports = async function(context, mySbMsg) {
     const keys = await redis.keys('*')
     context.log('keys', keys);
 
-  balance = parseFloat(await redis.get(mySbMsg.user_id))
+    subGetbalance = (a,b) => {
+        return a - b;
+    }
+
+    let result1 = subGetbalance(2,4);
+
+    context.log(result1);
+
+    addGetbalance = (a,b) => {
+        return a + b;
+    }
+
+    let result2 = addGetbalance(2,4);
+
+    context.log(result2);
+
+  let credit = 20;  
+  balance = parseInt(await redis.get(mySbMsg.user_id)) + credit;
+  context.log(balance + 1);
   context.log("current balance" + balance)
   switch (mySbMsg.action) {
     case "spending":
-      amount = balance - parseFloat(mySbMsg.amount);
+      amount = balance - parseInt(mySbMsg.amount);
       write = redis.set(mySbMsg.user_id, amount);
       context.log('Spending, new amount :'+ amount);
+      context.log(typeof(amount));
       break;
     case "payment":
-      amount = balance + parseFloat(mySbMsg.amount);
+      amount = balance + parseInt(mySbMsg.amount);
       write = redis.set(mySbMsg.user_id, amount);
       context.log('Payment, new amount :'+ amount);
       break;
